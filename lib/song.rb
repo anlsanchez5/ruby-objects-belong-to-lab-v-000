b050e0956062628f06b454f9e2640bff00294ded
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-01-25 19:59:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,9 @@
 
+<<<<<<< HEAD
 class Song
+=======
+class Song 
+>>>>>>> 4e079214293bf576ea556037bd2635a457229658
   attr_reader :title, :artist
 
   def title=(title)
@@ -9,4 +13,8 @@ class Song
   def artist=(artist)
     @artist = artist
   end
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 4e079214293bf576ea556037bd2635a457229658
